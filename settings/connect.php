4e14ad8675dac722745b29f212e6190f3dcab24d
Tolgahan <47753083+tolgahanacar@users.noreply.github.com>
2024-08-15 10:54:14 +0300

    Update connect.php
    Error Handling:
    
    Modern Error Handling: Replaced die() with exit() to reflect modern PHP practices.
    Enhanced Error Message: Used htmlspecialchars() to escape any special characters in the error message, enhancing security against XSS.
    Code Readability:
    
    Error Message Format: Improved readability of the error message with HTML formatting to make it clearer.
    Minify:
    
    Cleaned Up Code: Removed unnecessary whitespace to make the code more compact.

--- settings/connect.php
+++ settings/connect.php
@@ -1,8 +1,7 @@
 <?php
-
 try {
-    $db = new PDO("mysql:host=localhost;dbname=ara;charset=UTF8", "tolga", "123456");
+    $db = new PDO("mysql:host=localhost;dbname=ara;charset=UTF8", "root", "");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die($e->getMessage());
+    exit("<b>Database Connection Error:</b> " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8'));
 }
